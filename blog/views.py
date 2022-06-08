5e46c48b33a5070b9394575a2337bc035186db9c
Mohamedlee27 <leonk8070@gmail.com>
2022-06-08 11:21:58 +0300

    added imports in views.py and urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404,redirect
+from django.db.models import Q
+from django.http import HttpResponse,JsonResponse
+try:
+    from django.utils import simplejson as json
+except ImportError:
+    import json
+import random
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
